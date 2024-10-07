2a7f8698b34184c3ef2f2fd7a9d5a5250169d959
Jugal Kishore Nagulapalli <70318004+UserNJK@users.noreply.github.com>
2024-10-07 13:29:45 +0530

    Create replitTempRedirect.py

---
+++ replitTempRedirect.py
@@ -0,0 +1,18 @@
+# !pip intall flask
+# Paste below in replit main.py file
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "This is the home page."
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    return f"Authorization code: {code}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
